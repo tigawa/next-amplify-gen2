837828cbc4cf815a0922c7b4483f98ff631a5d4c
ta.igawa <ihciiat@gmail.com>
2023-11-26 17:56:07 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import {Amplify} from 'aws-amplify'
+import config from '@/amplifyconfiguration.json'
+import '@aws-amplify/ui-react/styles.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+Amplify.configure(config);
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default withAuthenticator(App);
